f20690022cdaef54bf7ced85ba512fa2f3cabbdd
OMJ- <mijames1@gmail.com>
2012-01-06 16:42:30 -0500

    lots o derps

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,5 +1,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/epic4gtouch.mk \
-    $(LOCAL_DIR)/fascinate.mk \
+    $(LOCAL_DIR)/fascinatemtd.mk \
     $(LOCAL_DIR)/toro.mk \
     $(LOCAL_DIR)/vivow.mk 
--- products/epic4gtouch.mk
+++ products/epic4gtouch.mk
@@ -5,7 +5,7 @@ $(call inherit-product, device/samsung/epic4gtouch/full_epic4gtouch.mk)
 $(call inherit-product, vendor/tsm/products/common.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := full_epic4gtouch
+PRODUCT_NAME := tsm_epic4gtouch
 PRODUCT_DEVICE := epic4gtouch
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
